e4c9ddaeb3c8d63a0588d4a547771225ab9ca67d
gpareraavoristravelcom <g.parera@avoristravel.com>
2021-09-11 01:38:29 +0200

    CLI  M keyboards/kprepublic/bm60rgb_iso/config.h  M keyboards/kprepublic/bm60rgb_iso/keymaps/parera10/keymap.c  M keyboards/kprepublic/bm60rgb_iso/rules.mk -- This message was automatically generated by 'git auto'

--- keyboards/kprepublic/bm60rgb_iso/config.h
+++ keyboards/kprepublic/bm60rgb_iso/config.h
@@ -56,19 +56,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     // #define RGBLIGHT_SAT_STEP 8
     // #define RGBLIGHT_VAL_STEP 8
 #    define RGBLIGHT_LIMIT_VAL 180 // Limit to vendor-recommended value
-    // #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+    #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
     /*== all animations enable ==*/
-        // #define RGBLIGHT_ANIMATIONS
+    //     #define RGBLIGHT_ANIMATIONS
     // /*== or choose animations ==*/
-    //     #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_BREATHING
     //     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
     //     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
     //     #define RGBLIGHT_EFFECT_SNAKE
-    //     #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_KNIGHT
     //     #define RGBLIGHT_EFFECT_CHRISTMAS
-    //     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
     //     #define RGBLIGHT_EFFECT_RGB_TEST
     //     #define RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_TWINKLE
 #endif
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180 // Limit to vendor-recommended value
@@ -76,3 +77,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM_PER_KEY
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_MAX_LAYERS 2
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
