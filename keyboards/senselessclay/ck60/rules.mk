050350a18a6e53ce81dfe25de8d90c7697a4e449
gpareraavoristravelcom <g.parera@avoristravel.com>
2021-12-23 01:49:33 +0100

    CLI A  keyboards/senselessclay/ck60/keymaps/parera10/keymap.c  M keyboards/senselessclay/ck60/rules.mk  M keyboards/ymd75/keymaps/parera10/keymap.c -- This message was automatically generated by 'git auto'

--- keyboards/senselessclay/ck60/rules.mk
+++ keyboards/senselessclay/ck60/rules.mk
@@ -16,3 +16,4 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+APPLE_FN_ENABLE = yes
