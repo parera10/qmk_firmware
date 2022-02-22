54a473fad9477abe4079ea04f7b6f2458a46ed27
gpareraavoristravelcom <g.parera@avoristravel.com>
2022-02-22 21:55:02 +0100

    CLI M  keyboards/senselessclay/ck60/keymaps/parera10/keymap.c  M lib/chibios -- This message was automatically generated by 'git auto'

--- keyboards/senselessclay/ck60/keymaps/parera10/keymap.c
+++ keyboards/senselessclay/ck60/keymaps/parera10/keymap.c
@@ -42,7 +42,7 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
         for (uint8_t i = led_min; i <= led_max; i++) {
             if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-                rgb_matrix_set_color(i, RGB_BLUE);
+                rgb_matrix_set_color(i, RGB_WHITE);
             }
         }
     }
