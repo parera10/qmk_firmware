4fa75bdc203123461a5b8be7ac62da465c556e30
parera10 <parera10@gmail.com>
2022-12-28 17:40:24 +0100

    CLI
    A  keyboards/senselessclay/ck60/keymaps/parera10/keymap.c
    M  keyboards/senselessclay/ck60/rules.mk
    A  keyboards/senselessclay/ck65/keymaps/parera10/keymap.c
    M  keyboards/senselessclay/ck65/rules.mk
    
    -- This message was automatically generated by 'git auto'
    
    Signed-off-by: parera10 <parera10@gmail.com>

--- keyboards/senselessclay/ck60/rules.mk
+++ keyboards/senselessclay/ck60/rules.mk
@@ -12,7 +12,7 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
@@ -20,4 +20,5 @@ AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes     # RGB matrix lighting
 RGB_MATRIX_DRIVER = WS2812
 
+APPLE_FN_ENABLE = yes
 LAYOUTS = 60_iso
